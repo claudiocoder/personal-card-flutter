77bd571c6a916c539e8a61811de66dcb39baa188
Claudio Moreno <clauartur98@gmail.com>
2020-04-24 21:16:04 -0500

    feat: exercise of layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,39 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              width: 100.0,
+              height: double.infinity,
+              color: Colors.red,
+            ),
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 100.0,
+              height: double.infinity,
+              color: Colors.blue,
+            ),
+          ],
+        )),
       ),
     );
   }
