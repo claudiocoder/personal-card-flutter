81317ae7124b3279730fcd243eca611905e08141
Claudio Moreno <clauartur98@gmail.com>
2020-04-24 22:56:14 -0500

    feat: add images and contact

--- lib/main.dart
+++ lib/main.dart
@@ -9,40 +9,69 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.red.shade600,
         body: SafeArea(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              width: 100.0,
-              height: double.infinity,
-              color: Colors.red,
-            ),
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.green,
-                  ),
-                ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 70.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/avatar.jpg'),
               ),
-            ),
-            Container(
-              width: 100.0,
-              height: double.infinity,
-              color: Colors.blue,
-            ),
-          ],
-        )),
+              Text(
+                'Claudio Moreno',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Software Developer',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Sans Pro',
+                    color: Colors.grey.shade100,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.red),
+                  title: Text(
+                    '55-XXXX-7180',
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontFamily: 'Sans Pro',
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.red.shade600),
+                  title: Text(
+                    'myemail@gmail.com',
+                    style: TextStyle(
+                        color: Colors.red.shade600,
+                        fontFamily: 'Sans Pro',
+                        fontSize: 20.0),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
